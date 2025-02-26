9f7df05fef211b96dd440f8fd6a820680356e30a
Leandro <leanfiadone@gmail.com>
2025-02-25 22:35:16 -0300

    cambios al llamado de la API

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/auth/",
-    // withCredentials: true,
-});
+  baseURL:
+    import.meta.env.VITE_API_URL 
+  // withCredentials: true,
+})
 
-export const googleAuth = (code) => api.get(`/google?code=${code}`);
+export const googleAuth = (code) => api.get(`/google?code=${code}`)
