cbfcb074aa6fa45fda0c48861fc5a1c3ee578fd0
Leandro <leanfiadone@gmail.com>
2025-02-27 20:25:07 -0300

    loading y refreshandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,66 @@
+import {GoogleOAuthProvider} from "@react-oauth/google"
+import GoogleLogin from "./GoogleLogin"
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
+import Dashboard from "./Dashboard"
+import {useState} from "react"
+import PropTypes from "prop-types"
+import RefreshHandler from "./RefreshHandler"
+import NotFound from "./NotFound"
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import GoogleLogin from './GoogleLogin';
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
-import Dashboard from './Dashboard';
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import RefrshHandler from './RefreshHandler';
-import NotFound from './NotFound';
-
-
+// Componente de carga
+const LoadingScreen = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    }}>
+    <h2>Cargando perfil...</h2>
+  </div>
+)
 
 function App() {
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const GoogleWrapper = () => (
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isLoadingUser, setIsLoadingUser] = useState(true) // Estado de carga del usuario
+
+  const handleUserLoaded = (authStatus) => {
+    setIsAuthenticated(authStatus)
+    setIsLoadingUser(false) // Se desactiva el loading en cualquier caso
+  }
+
+  const GoogleWrapper = () => (
     <GoogleOAuthProvider clientId="677835323710-huch33bpc7g8nt8qnk1ab6jkjnij8l3n.apps.googleusercontent.com">
-      <GoogleLogin></GoogleLogin>
+      <GoogleLogin />
     </GoogleOAuthProvider>
   )
 
+  const PrivateRoute = ({element}) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
 
+  PrivateRoute.propTypes = {
+    element: PropTypes.node.isRequired
+  }
 
-
-const PrivateRoute = ({element}) => {
-  return isAuthenticated ? element : <Navigate to="/login" />
-}
-
-PrivateRoute.propTypes = {
-  element: PropTypes.node.isRequired
-}
-
-
-	return (
-		<BrowserRouter>
-			<RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-			<Routes>
-				<Route path="/login" element={<GoogleWrapper />} />
-				<Route path="/" element={<Navigate to="/login" />} />
-				<Route path='/dashboard' element={<PrivateRoute element={<Dashboard/>}/>}/>
-				<Route path="*" element={<NotFound/>} />
-			</Routes>
-	</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <RefreshHandler setIsAuthenticated={handleUserLoaded} />
+      {isLoadingUser ? (
+        <LoadingScreen /> // Solo muestra la pantalla de carga si el usuario aún no se ha verificado
+      ) : (
+        <Routes>
+          <Route path="/login" element={<GoogleWrapper />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route
+            path="/dashboard"
+            element={<PrivateRoute element={<Dashboard />} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/RefreshHandler.js
+++ src/RefreshHandler.js
@@ -1,26 +1,30 @@
-import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useEffect} from "react"
+import PropTypes from 'prop-types';
+import {useLocation, useNavigate} from "react-router-dom"
 
-function RefrshHandler({ setIsAuthenticated }) {
-    const location = useLocation();
-    const navigate = useNavigate();
+function RefreshHandler({setIsAuthenticated}) {
+  const location = useLocation()
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        const data = localStorage.getItem('user-info');
-        const token = JSON.parse(data)?.token;
-        if (token) {
-            setIsAuthenticated(true);
-            if (location.pathname === '/' ||
-                location.pathname === '/login'
-            ) {
-                navigate('/dashboard', { replace: false });
-            }
-        }
-    }, [location, navigate, setIsAuthenticated])
+  useEffect(() => {
+    const data = localStorage.getItem("user-info")
+    const token = data ? JSON.parse(data)?.token : null
 
-    return (
-        null
-    )
+    if (token) {
+      setIsAuthenticated(true)
+      if (location.pathname === "/" || location.pathname === "/login") {
+        navigate("/dashboard", {replace: false})
+      }
+    } else {
+      setIsAuthenticated(false)
+    }
+  }, [location, navigate, setIsAuthenticated])
+
+  return null
 }
+RefreshHandler.propTypes = {
+  setIsAuthenticated: PropTypes.func.isRequired,
+};
 
-export default RefrshHandler
+
+export default RefreshHandler
